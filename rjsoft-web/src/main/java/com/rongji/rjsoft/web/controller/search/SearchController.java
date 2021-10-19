a691bf387442670b3221f1087d72f67a3e54d9c9
JohnYehyo <836524969@qq.com>
2021-10-19 13:27:19 +0800

    feat: 搜索详情

--- rjsoft-web/src/main/java/com/rongji/rjsoft/web/controller/search/SearchController.java
+++ rjsoft-web/src/main/java/com/rongji/rjsoft/web/controller/search/SearchController.java
@@ -1,6 +1,8 @@
 package com.rongji.rjsoft.web.controller.search;
 
+import com.rongji.rjsoft.entity.monitor.SysOperationLog;
 import com.rongji.rjsoft.query.search.SearchPageQuery;
+import com.rongji.rjsoft.query.search.SearchQuery;
 import com.rongji.rjsoft.service.IEsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,9 +26,28 @@ public class SearchController {
 
     private final IEsService esService;
 
+    /**
+     * 多条件搜索
+     * @param searchPageQuery 条件对象
+     * @return 分页结果
+     */
     @ApiOperation(value = "多条件搜索")
     @GetMapping(value = "list")
     public Object list(@Valid SearchPageQuery searchPageQuery) {
+        searchPageQuery.setIndexName("basic_log_index");
+        searchPageQuery.setIndexType("doc");
+        searchPageQuery.setClazz(SysOperationLog.class);
         return esService.queryForlist(searchPageQuery);
+    }
+
+    /**
+     * 详情
+     * @param searchQuery 条件对象
+     * @return 分页结果
+     */
+    @ApiOperation(value = "搜索详情")
+    @GetMapping(value = "info")
+    public Object list(@Valid SearchQuery searchQuery) {
+        return esService.queryForEntity(searchQuery);
     }
 }
