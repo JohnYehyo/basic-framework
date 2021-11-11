f8478b801a592631fad9a7a39b37ef65479a0147
JohnYehyo <836524969@qq.com>
2021-11-11 14:25:35 +0800

    feat: 部门同步树列表

--- rjsoft-core/src/main/java/com/rongji/rjsoft/vo/system/dept/SysDeptAllTreeVo.java
+++ rjsoft-core/src/main/java/com/rongji/rjsoft/vo/system/dept/SysDeptAllTreeVo.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class SysDeptAllTreeVo implements Serializable {
 
     private static final long serialVersionUID = 676873841324848348L;
+
     /**
      * 部门id
      */
@@ -30,7 +31,7 @@ public class SysDeptAllTreeVo implements Serializable {
     private String deptName;
 
     /**
-     * 是否叶子节点
+     * 子节点
      */
     private List<SysDeptAllTreeVo> children;
 }
