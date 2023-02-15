f251a491479f64873022ef049b400db1e3bd4241
johnyehyo <JohnYehyo@hotmail.com>
2023-02-15 13:03:24 +0800

    chore(mq): fanout模式(切queue为空让系统自动生成, 从而所有队列都接到消息)

---
+++ rjsoft-web/src/main/java/com/rongji/rjsoft/web/component/RabbitMqReceiver6.java
@@ -0,0 +1,57 @@
+package com.rongji.rjsoft.web.component;
+
+import com.rabbitmq.client.Channel;
+import com.rongji.rjsoft.common.util.LogUtils;
+import com.rongji.rjsoft.enums.RabbitResult;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @description: rabbitmq消费
+ * @author: JohnYehyo
+ * @create: 2022-01-13 11:10:47
+ */
+@Component
+public class RabbitMqReceiver6 {
+
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(),
+            exchange = @Exchange(value = "am-fanout-ex", durable = "true", type = ExchangeTypes.FANOUT)
+    ), concurrency = "10")
+    public void onMessage(@Payload String body, @Headers Map<String, Object> headers, Channel channel) throws IOException {
+        Long tag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        String correlationId = (String) headers.get("spring_returned_message_correlation");
+        int prefecthSize = 0;
+        int prefetchCount = 30;
+        boolean global = false;
+        channel.basicQos(prefecthSize, prefetchCount, global);
+        RabbitResult rr = RabbitResult.RETRY;
+        try {
+            LogUtils.info("6消费消息:{}", body);
+            rr = RabbitResult.SUCCESS;
+        } catch (Exception e) {
+            rr = RabbitResult.DISCARDED;
+            LogUtils.error("业务逻辑处理错误");
+        } finally {
+            if (rr == RabbitResult.SUCCESS) {
+                //告诉服务器收到这条消息 无需再发了 否则消息服务器以为这条消息没处理掉 后续还会在发
+                channel.basicAck(tag, false);
+            } else if (rr == RabbitResult.RETRY) {
+                channel.basicNack(tag, false, true);
+            } else {
+                channel.basicNack(tag, false, false);
+            }
+        }
+    }
+}
