5c1081c4f5ab8514af6cea2bb555b7dc0d22af76
JohnYehyo <johnyehyo@hotmail.com>
2024-07-04 18:59:09 +0800

    style: 删除无用引用

--- rjsoft-web/src/main/java/com/rongji/rjsoft/web/config/ThreadPoolConfig.java
+++ rjsoft-web/src/main/java/com/rongji/rjsoft/web/config/ThreadPoolConfig.java
@@ -1,7 +1,6 @@
 package com.rongji.rjsoft.web.config;
 
 import com.rongji.rjsoft.web.handler.MyRejectedExecutionHandler;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
