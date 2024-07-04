615b6df4b65292eec4051bbe65677af029607783
JohnYehyo <johnyehyo@hotmail.com>
2024-07-04 18:33:49 +0800

    feat: 多线程操作的一些恢复训练

--- rjsoft-web/src/main/java/com/rongji/rjsoft/web/practice3/NotificationSystemApplicationRunner.java
+++ rjsoft-web/src/main/java/com/rongji/rjsoft/web/practice3/NotificationSystemApplicationRunner.java
@@ -16,8 +16,8 @@ import java.util.stream.Collectors;
 
 /**
  * @description: 1. 系统启动时启动通知发送和接收的监听
- *               2. 使用自定线程池
- *               3. 执行无返回和有返回值的任务, 有返回值的任务进行模拟合并结果
+ * 2. 使用自定线程池
+ * 3. 执行无返回和有返回值的任务, 有返回值的任务进行模拟合并结果
  * @author: JohnYehyo
  * @create: 2024-07-04 14:54:28
  */
@@ -48,6 +48,17 @@ public class NotificationSystemApplicationRunner implements ApplicationRunner {
             }, threadPoolTaskExecutor);
         }
 
+        /*
+            体会以下两种有返回值异步任务对结果处理的区别:
+            第一种会双阻塞(第一次get阻塞是为了给全部任务线程限制总时间, 第二次join是为了获取结果)
+            第二种不会双阻塞(只有get), 但是可能因为线程池的设置(线程池被耗尽, 实测调大核心线程的确
+            可以减少这种情况)导致最后结果比总线程少,本例中如果循环是100collect2应该有100个结果但
+            是collect2的size经常小于100:
+            每个future一完成时就立即加入到collect2中，无论完成是否成功。然而，由于这些操作是异步的，
+            并且外部循环在等待之前的任务完成之前就继续提交新的任务，如果主线程在所有future都有机会将
+            它们的结果加入到collect2之前就完成了它的执行，那么collect2可能会收到少于100个条目
+         */
+
         //有返回值的异步任务
         List<CompletableFuture<String>> list = new ArrayList<>(threadPoolTaskExecutor.getMaxPoolSize());
         for (int i = 0; i < threadPoolTaskExecutor.getMaxPoolSize(); i++) {
@@ -61,9 +72,32 @@ public class NotificationSystemApplicationRunner implements ApplicationRunner {
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             throw new RuntimeException(e);
         }
-
         List<String> collect = list.stream().map(CompletableFuture::join).collect(Collectors.toList());
-        LogUtils.info("collect:{}", JSON.toJSONString(collect));
+        LogUtils.info("collect:{}, 长度:{}", JSON.toJSONString(collect), collect.size());
+
+        //有返回值的异步任务2
+        List<CompletableFuture<String>> list2 = new ArrayList<>(threadPoolTaskExecutor.getMaxPoolSize());
+        List<String> collect2 = new ArrayList<>();
+        for (int i = 0; i < threadPoolTaskExecutor.getMaxPoolSize(); i++) {
+            list2.add(CompletableFuture.supplyAsync(() ->
+                            RandomUtil.randomString(10)
+                    , threadPoolTaskExecutor).handle((s, throwable) -> {
+                if (throwable != null) {
+                    LogUtils.error("异常:{}", throwable.getMessage());
+                    return null;
+                } else {
+                    collect2.add(s);
+                    return s;
+                }
+            }));
+        }
+        CompletableFuture<Void> completableFuture2 = CompletableFuture.allOf(list2.toArray(new CompletableFuture[0]));
+        try {
+            completableFuture2.get(20, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+        LogUtils.info("collect2:{}, 长度:{}", JSON.toJSONString(collect2), collect2.size());
 
         //启动通知收发线程
         CompletableFuture.runAsync(sender::action, threadPoolTaskExecutor);
